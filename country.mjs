677b32ba284393f2dff048ed7a3c9ee93fa5b21e
Ryan Harris <harris.ucla@gmail.com>
2020-03-29 14:59:42 -0700

    remove 2nd call of render country data

--- country.mjs
+++ country.mjs
@@ -14,7 +14,6 @@ function init() {
   buildNewsArticles(countryObj);
   renderSpecificCountryName(countryObj.display);
   renderSpecificCountryData(countryObj.country);
-  renderSpecificCountryData(countryObj.country);
   setInterval(function() {
     renderSpecificCountryData(countryObj.country);
   }, 600000);
